812b1270db26a64e26ebc3391ee31ca9ba9c8d15
InControl001 <jo.van.montfort1@telenet.be>
2015-02-08 20:02:56 +0100

    finished main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-class Box{
+class Box
+{
 	public:
 		Box(double l=2.0, double b=2.0, double h=2.0)
 		{
@@ -25,8 +26,19 @@ class Box{
 		double height;
 };
 
-int main(){
+int main()
+{
 	Box Box1(3.3, 1.2, 1.5);
 	Box Box2(8.5, 6.0, 2.0);
+
+	if(Box1.compare(Box2))
+	{
+		cout << "Box2 is smaller than Box1" <<endl;
+	}
+	else
+	{
+		cout << "Box2 is equal to or larger than Box1" <<endl;
+	}
+	return 0;
 
 }
